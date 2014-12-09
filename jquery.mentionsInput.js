b16b6af20c8cbfe9d59f0f219316c227b6ebfee1
André Gil <contato@andregil.org>
2014-12-09 11:24:26 -0200

    IE8 won't fire the "input" event on textarea, so bind to "propertychange" to make it work

--- jquery.mentionsInput.js
+++ jquery.mentionsInput.js
@@ -98,9 +98,14 @@
       elmInputBox.attr('data-mentions-input', 'true'); //Sets the attribute data-mentions-input to true -> Defines if the text area is already configured
       elmInputBox.bind('keydown', onInputBoxKeyDown); //Bind the keydown event to the text area
       elmInputBox.bind('keypress', onInputBoxKeyPress); //Bind the keypress event to the text area
-      elmInputBox.bind('input', onInputBoxInput); //Bind the input event to the text area
       elmInputBox.bind('click', onInputBoxClick); //Bind the click event to the text area
       elmInputBox.bind('blur', onInputBoxBlur); //Bind the blur event to the text area
+
+      if (navigator.userAgent.indexOf("MSIE 8") > -1) {
+        elmInputBox.bind('propertychange', onInputBoxInput); //IE8 won't fire the input event, so let's bind to the propertychange
+      } else {
+        elmInputBox.bind('input', onInputBoxInput); //Bind the input event to the text area
+      }
 
       // Elastic textareas, grow automatically
       if( settings.elastic ) {
